6bbfd837891425fed93eea90d989cc5214d84fa6
Tian-Chris <tianchris9810@gmail.com>
2025-04-08 18:38:19 -0700

    removed build which i accidentally added

--- build/CMakeFiles/cttss.dir/global/globals.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cttss.dir/global/globals.cpp.o: \
-  /Users/chris/cttss/global/globals.cpp \
-  /Users/chris/cttss/global/globals.h
--- build/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chris/cttss/build/_deps/flac-src/microbench
-# Build directory: /Users/chris/cttss/build/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
